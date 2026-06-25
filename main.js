1676674e14d38d1e54046ac363e580b90f39e0c8
Jimli <j.kachelries@gmail.com>
2026-06-25 12:28:34 -0400

    Added functions for getting Computer and Human choices

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+console.log("No peeking, please.");
+
+function getComputerChoice() {    
+    const randomPicker = Math.floor(Math.random() * 3);
+    const rock = "Rock";
+    const paper = "Paper";
+    const scissor = "Scissor";
+
+    if (randomPicker === 0) {
+        return rock;
+    } else if (randomPicker === 1) {
+        return paper;
+    } else if (randomPicker === 2) {
+        return scissor;
+    }
+}
+
+function getHumanChoice() {
+    const choice = String(prompt("What's it going to be?", "Enter"));
+    return choice;
+}
